b9cc9367052325a6f9380afcf9c28ba610d8b899
aitseb <lchlebda@gmail.com>
2023-05-18 18:40:49 +0200

    send info to other microbit

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
-let isAlarmStopped = false
-let distance = 0
 input.onButtonPressed(Button.A, function () {
     isAlarmStopped = true
 })
+let distance = 0
+let isAlarmStopped = false
+radio.setGroup(1)
 basic.forever(function () {
     distance = sonar.ping(
     DigitalPin.P0,
@@ -10,7 +11,10 @@ basic.forever(function () {
     PingUnit.Centimeters
     )
     if (distance < 50 && distance != 0 && !(isAlarmStopped)) {
+        radio.sendString("AlarmOn")
         music.playTone(988, music.beat(BeatFraction.Whole))
         basic.pause(100)
+    } else {
+        radio.sendString("AlarmOf")
     }
 })
